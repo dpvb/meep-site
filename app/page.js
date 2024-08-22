c6f4371e58959b0afc669675c1de41ffbc98079b
d-lee-te <dlee.ggwp@gmail.com>
2024-08-21 22:36:05 -0400

    Title Gradient

--- app/page.js
+++ app/page.js
@@ -12,7 +12,8 @@ export default async function Home() {
     <main className="flex flex-col w-3/4 mx-auto items-center">
       <div className="flex mt-8 items-center gap-4">
         <ChromaMeep/>
-        <p className="text-7xl mt-[10px] font-bold bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500 w-min bg-clip-text text-transparent">
+        <p className="text-7xl mt-[10px] font-bold bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500 w-min bg-clip-text text-transparent animate-rainbow"
+        style={{ backgroundSize: '200% 200%' }}>
           Meep</p>
       </div>
       <div className="flex flex-col md:flex-row items-start gap-4">
--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -6,7 +6,7 @@ export default function Leaderboard({ stats }) {
     return (
         <div className="flex-col w-72 border-neutral-800 bg-neutral-900 border-2 rounded-md mt-8 pt-2 px-5" >
             <h1 className="font-semibold text-xl text-center">Leaderboard</h1>
-            <table class="table-auto w-full mt-2">
+            <table className="table-auto w-full mt-2">
                 <thead className="border-b rounded-md border-neutral-800 ">
                     <tr>
                         <th className= "text-left text-neutral-300">User</th>
